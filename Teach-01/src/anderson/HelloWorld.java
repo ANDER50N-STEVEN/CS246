6f7a8f28765535fea82a082815185d0b29a8ef45
stevoanderson <and12128@byui.edu>
2018-01-31 13:11:08 -0700

    Add week 1 in class assignment

---
+++ Teach-01/src/anderson/HelloWorld.java
@@ -0,0 +1,12 @@
+package anderson;
+
+public class HelloWorld {
+    public void sayHello(){
+        System.out.println("Hello World!");
+    }
+    public static void main(String[] args) {
+	// write your code here
+        HelloWorld hello = new HelloWorld();
+        hello.sayHello();
+    }
+}
